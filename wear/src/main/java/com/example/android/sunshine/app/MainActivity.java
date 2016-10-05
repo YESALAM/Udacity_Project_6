50f7a1062a4086599df83c08261ac74ca87da944
Sadare Alam <itisalam@gmail.com>
2016-10-05 21:48:30 +0530

    Add data item listener to wear Activity

--- wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ wear/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.android.sunshine.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+
+public class MainActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, DataApi.DataListener{
 
     private TextView mTextView;
+    private String KEY = "key" ;
+    private GoogleApiClient mGoogleApiClient ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,57 @@ public class MainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGoogleApiClient.disconnect();
+        Wearable.DataApi.removeListener(mGoogleApiClient,this) ;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Wearable.DataApi.addListener(mGoogleApiClient,this) ;
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEventBuffer) {
+        for(DataEvent event:dataEventBuffer){
+            if(event.getType()==DataEvent.TYPE_CHANGED){
+                //DataItem changed
+                DataItem item = event.getDataItem() ;
+                if(item.getUri().getPath().compareTo("/num")==0){
+                    DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
+                    //updateTV(dataMap.getString(KEY));
+                    mTextView.setText(dataMap.getString(KEY));
+                }
+            }
+        }
+    }
+
+
 }
